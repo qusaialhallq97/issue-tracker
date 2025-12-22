dcb4b8339c68259daf87f998cedd567702b57578
qusaialhallq97 <qusai.abd.alhallaq@gmail.com>
2025-12-22 14:10:51 +0300

    feat: refactor issue creation validation and update form handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 import axios from 'axios';
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
 import { set, useForm } from 'react-hook-form';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '../../validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = React.useState('');
   return (
     <div className='max-w-xl'>
@@ -35,7 +41,17 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
+        )}
         <TextArea placeholder='Description' {...register('description')} />
+        {errors.description && (
+          <Text color='red' as='p'>
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
