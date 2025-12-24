0753230e55c958dc2e59aad9d2ca3552af40c3d4
qusaialhallq97 <qusai.abd.alhallaq@gmail.com>
2025-12-24 17:52:01 +0300

    styling issue link

---
+++ app/components/Link.tsx
@@ -0,0 +1,16 @@
+import NextLink from 'next/link';
+import { Link as RadixLink } from '@radix-ui/themes';
+
+interface LinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+const Link = ({ href, children }: LinkProps) => {
+  return (
+    <NextLink href={href} passHref legacyBehavior>
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  );
+};
+
+export default Link;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Table } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import delay from 'delay';
 import IssueAcations from './IssueAcations';
+import Link from '../components/Link';
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
   await delay(2000);
