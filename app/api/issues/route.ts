1604c48716b5026266ee0e4693a438d1e1a3c02b
qusaialhallq97 <qusai.abd.alhallaq@gmail.com>
2025-12-26 16:08:15 +0300

    add patch issue api

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,10 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
-import { createIssueSchema } from '../../validationSchemas';
+import { issueSchema } from '../../validationSchemas';
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validationResult = createIssueSchema.safeParse(body);
+  const validationResult = issueSchema.safeParse(body);
 
   if (!validationResult.success) {
     return NextResponse.json(validationResult.error.errors, {
